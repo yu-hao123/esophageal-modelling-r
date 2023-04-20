1b6ec2355b665dd4a05165f6c0fb536f63009171
yu-hao123 <yuhaowangxia@gmail.com>
2023-04-20 19:36:31 -0300

    feat: adds a filter step before applying the transfer function

--- example.R
+++ example.R
@@ -2,12 +2,17 @@ library(ggplot2)
 library(plotly)
 library(sysid)
 
+rm(list = ls())
 source("transfer.R")
 
 df <- read.table("simple.csv", header = TRUE, sep = ",")
 gain <- 1.1096
-esophageal <- df$pes
+esophageal_raw <- df$pes
 tf_pes <- df$tf_pes
+
+#bf = butter(5, 0.06)
+#esophageal <- filtfilt(bf, esophageal_raw)
+esophageal <- lowpass_esophageal(esophageal_raw)
 
 result <- oe_model(esophageal, gain)
 
@@ -15,7 +20,8 @@ x <- seq_along(esophageal)
 
 esophageal_df <- data.frame(x, esophageal, result, tf_pes)
 
-fig <- plot_ly(dataf, y = ~esophageal, type = 'scatter', name = 'original pes', mode = 'lines')
+fig <- plot_ly(esophageal_df, y = ~esophageal, type = 'scatter', name = 'filtered pes', mode = 'lines')
+fig <- fig %>% add_trace(y = ~esophageal_raw, name = 'original pes (MATLAB)', mode = 'lines')
 fig <- fig %>% add_trace(y = ~tf_pes, name = 'corrected pes (MATLAB)', mode = 'lines')
 fig <- fig %>% add_trace(y = ~result, name = 'corrected pes (R)', mode = 'lines')
 fig <- fig %>% layout(legend = list(x = 0.05, y = 0.05))
--- transfer.R
+++ transfer.R
@@ -1,4 +1,18 @@
 library(sysid)
+library(signal)
+
+lowpass_esophageal <- function(esophageal_pressure) {
+    # Filter parameters
+    fs <- 100
+    order <- 16 # should be much lower than the input data size
+    HR <- 60
+    cutoff <- HR / 5
+    wn <- cutoff / (fs / 2)
+
+    filter_coefs <- fir1(order, wn, "low", scale = FALSE)
+    filtered <- filtfilt(filter_coefs, esophageal_pressure)
+    return(filtered)
+}
 
 oe_model <- function(input_signal, gain) {
     model <- idpoly(A = 1, B = c(0, 20.9958, -19.4621),
@@ -7,3 +21,4 @@ oe_model <- function(input_signal, gain) {
     output_signal <- gain * sim(model, input_signal)
     return(output_signal)
 }
+
